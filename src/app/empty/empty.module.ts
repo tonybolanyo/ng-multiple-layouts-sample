4de361494bcf8431ca0a98884576eebaa1745961
Tony G. Bolaño <tonybolanyo@gmail.com>
2020-06-02 12:48:19 +0200

    chore: add modules

---
+++ src/app/empty/empty.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmptyRoutingModule } from './empty-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EmptyRoutingModule
+  ]
+})
+export class EmptyModule { }
---
+++ src/app/frontend/frontend.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FrontendRoutingModule } from './frontend-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FrontendRoutingModule
+  ]
+})
+export class FrontendModule { }
